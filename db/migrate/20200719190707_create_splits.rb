d575b94c0e4a87772057dc9e660d21f11abb816a
Wills Blake <richwblake@gmail.com>
2020-07-19 15:08:41 -0400

    Created split model/migration and migrated to db

---
+++ db/migrate/20200719190707_create_splits.rb
@@ -0,0 +1,9 @@
+class CreateSplits < ActiveRecord::Migration[6.0]
+  def change
+    create_table :splits do |t|
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_022718) do
+ActiveRecord::Schema.define(version: 2020_07_19_190707) do
 
   create_table "games", force: :cascade do |t|
     t.string "name"
     t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "splits", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
